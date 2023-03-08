c67b52533ecd65f247a9f693b9592dc1c175f21c
Kristen Schaefer <kristencschaefer@gmail.com>
2023-03-08 14:15:13 -0600

    added books and user models and actually saved this time

--- server/models/User.js
+++ server/models/User.js
@@ -1,6 +1,36 @@
 const { Schema, model } = require('mongoose');
 
 const userSchema = new Schema({
+  first_name: {
+    type: String,
+    required: true,
+  },
+
+  last_name: {
+    type: String,
+    required: true,
+  },
+  
+  address: {
+    type: String,
+    required: true,
+  },
+
+  city: {
+    type: String,
+    required: true,
+  },
+
+  state: {
+    type: String,
+    required: true,
+  },
+
+  zip_code: {
+    type: String,
+    required: true,
+  },
+
   email: {
     type: String,
     required: true,
