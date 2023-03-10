2881cf7fbc4ff2dd790d7395822f34ce614505bb
tesseir <tesseir.edu@gmail.com>
2023-03-10 11:16:27 -0600

    merged signup and login

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { AppProvider } from "./utils/AppContext";
 import { Header, Wrapper } from "./components"
-import { HomePage, LoginPage, ProfilePage, SignupPage } from "./pages";
+import { HomePage, LoginSignupPage, ProfilePage} from "./pages";
 
 // import 'bootstrap/dist/css/bootstrap.min.css';
 // import './styles/global.css'
@@ -15,9 +15,9 @@ function App() {
           <div className="pt-3 px-4">
             <Routes>
               <Route path="/" element={<HomePage />} />
-              <Route path="/login" element={<LoginPage />} />
+              <Route path="/login" element={<LoginSignupPage />} />
               <Route path="/profile" element={<ProfilePage />} />
-              <Route path="/signup" element={<SignupPage />} />
+              <Route path="/signup" element={<LoginSignupPage />} />
               <Route path="*" element={<div>404</div>} />
             </Routes>
           </div>
--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -2,6 +2,5 @@
 
 
 export { default as HomePage } from "./HomePage"
-export { default as LoginPage } from "./LoginPage"
+export { default as LoginSignupPage } from "./LoginSignupPage"
 export { default as ProfilePage } from "./ProfilePage"
-export { default as SignupPage } from "./SignupPage"
