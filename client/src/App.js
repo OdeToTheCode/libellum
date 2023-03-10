346d331e85c3796a1f3229a9daca0a9162beafa9
Kristen Schaefer <kristencschaefer@gmail.com>
2023-03-10 13:01:25 -0600

    working to get search results to render on search page

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,30 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { AppProvider } from "./utils/AppContext";
-import { Header, Wrapper } from "./components"
+import { Header, Navigation, Wrapper } from "./components"
 import { HomePage, SearchPage, LoginSignup } from "./pages";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "./assets/css/app.css"
+import { useState, useEffect } from "react"
 
 // import 'bootstrap/dist/css/bootstrap.min.css';
 // import './styles/global.css'
 
 function App() {
+  const [bookData, setBookData] = useState([]);
+  useEffect(() => {
+    
+  },[bookData])
 
   return (
     <AppProvider>
       <BrowserRouter>
+        <Navigation bookData={bookData} setBookData={setBookData}/>
         {/* <Wrapper> */}
           <div className="pt-3 px-4">
             <Routes>
               <Route path="/" element={<HomePage />} />
               <Route path="/login" element={<LoginSignup />} />
-              <Route path="/expolore" element={<SearchPage />} />
+              <Route path="/explore" element={<SearchPage bookData={bookData} setBookData={setBookData}/>} />
               <Route path="*" element={<div>404</div>} />
             </Routes>
           </div>
--- client/src/pages/SearchPage.js
+++ client/src/pages/SearchPage.js
@@ -1,23 +1,27 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import { useAppCtx } from "../utils/AppContext"
 import useApi from "../utils/useApi"
 import { BookSearch, Navigation } from "../components"
 
 
-const SearchPage = () => {
-
+const SearchPage = ({ bookData} ) => {
+  const [ bookDataSearch, setBookDataSearch ] = useState([])
+  useEffect(() => {
+    setBookDataSearch(bookData)
+    console.log(bookDataSearch)
+  },[bookData])
 
   return (
     <>
-      < Navigation />
 
-      {/* <div>
-          {(bookData.length !== 0) && (<p>{bookData[0].volumeInfo.title}</p>)}
-          {(bookData.length !== 0) && (<p>{bookData[0].volumeInfo.subtitle}</p>)}
-          {(bookData.length !== 0) && (<p>{bookData[0].volumeInfo.authors[0]}</p>)}
-          {(bookData.length !== 0) && (<p>{bookData[0].searchInfo.textSnippet}</p>)}
-          {(bookData.length !== 0) && (<img src={bookData[0].volumeInfo.imageLinks.thumbnail} />)}
-        </div> */}
+
+      <div>
+          {(bookDataSearch.length !== 0) && (<p>{bookDataSearch[0].volumeInfo.title}</p>)}
+          {(bookDataSearch.length !== 0) && (<p>{bookDataSearch[0].volumeInfo.subtitle}</p>)}
+          {(bookDataSearch.length !== 0) && (<p>{bookDataSearch[0].volumeInfo.authors[0]}</p>)}
+          {(bookDataSearch.length !== 0) && (<p>{bookDataSearch[0].searchInfo.textSnippet}</p>)}
+          {(bookDataSearch.length !== 0) && (<img src={bookDataSearch[0].volumeInfo.imageLinks.thumbnail} />)}
+        </div>
     </>
   )
 }
