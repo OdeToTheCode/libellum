4ac2fd623445c1a2df631f4ce3f1568c642f5889
Kristen Schaefer <kristencschaefer@gmail.com>
2023-03-12 17:08:49 -0500

    formatting explore search results

--- client/src/components/BookSearch.jsx
+++ client/src/components/BookSearch.jsx
@@ -5,11 +5,14 @@ import Container from 'react-bootstrap/Container';
 import defaultBookImg from "../assets/images/book.jpeg";
 
 
+function randomIntFromInterval(min, max) { 
+  return (Math.random() * (max - min + 1) + min).toFixed(2)
+}
+
+// BookSearch function and return below ---------------------------
+
 const BookSearch = ({ bookData, setBookData }) => {
-
-  // may need to add this back into the booksearch function as a prop
-  // , search, setSearch, FetchBookData 
-
+  
   const [search, setSearch] = useState("");
 
   const handleInputChange = (e) => {
@@ -19,14 +22,15 @@ const BookSearch = ({ bookData, setBookData }) => {
   const update = async (e) => {
     e.preventDefault();
    
-    
+// API call for Google Books - returns and maps an array to serve on the search page -----
+
     const resp = await fetch(`https://www.googleapis.com/books/v1/volumes?q=${search}`);
     const data = await resp.json();
     const returnedData = data.items.map (item => {
+      const rndInt = randomIntFromInterval(10, 50)
       const authors = item.volumeInfo.authors ?? [] ;
       const imageSource = item.volumeInfo.imageLinks ?? {
-        thumbnail: defaultBookImg
-      };
+        thumbnail: defaultBookImg};
       return {
         id: item.id,
         title: item.volumeInfo.title,
@@ -34,14 +38,14 @@ const BookSearch = ({ bookData, setBookData }) => {
         authors: authors,
         description: item.volumeInfo.description,
         image: imageSource.thumbnail,
-        price: "$15.99",
+        price: `$${rndInt}`,
       }
     })
     setBookData(returnedData);
     console.log(bookData);
-    // fetchBookData()
   }
 
+// returned JSX for search bar functionality -------------------------------
 
   return (
     <div>
--- client/src/pages/SearchPage.js
+++ client/src/pages/SearchPage.js
@@ -1,38 +1,55 @@
-import { useState, useEffect } from "react"
-import { useAppCtx } from "../utils/AppContext"
-import useApi from "../utils/useApi"
-import { BookSearch, Navigation } from "../components"
+// import { useState, useEffect } from "react"
+// import { useAppCtx } from "../utils/AppContext"
+// import useApi from "../utils/useApi"
+// import { BookSearch, Navigation } from "../components"
+import "../assets/css/explore.css"
 
 
 const SearchPage = ({ bookData }) => {
 
   return (
-    <>
+    <section>
+      {bookData.length>0 ? <h1>Search Results</h1> : ""}
+      {bookData.map(book => {
+        return (
+          <div className="searchFlex">
+            <div className="topSearchFlex">
+              <div className="infoFlex">
+                <div key={book.id}>
+                  <div><h2>{book.title}</h2></div>
+                  <div><h4>{book.subtitle}</h4></div>
+                  <img src={book.image} alt="book cover art" />
+                </div>
+              </div>
+              <div className="priceSearchFlex">
+                <div>{book.price}</div>
+                <button>Add to Cart</button>
+              </div>
 
-
-      <section>
-        {bookData.map(book => {
-          return (
-            <div key={book.id}>
-              <div>{book.title}</div>
-              <div>{book.subtitle}</div>
-              <div>{book.authors.map(author => {
-                return (
-                  <div key={author}>
-                    <div>{author}</div>
-                  </div>
-                )
-              })}
+            </div>
+            <div className="bottomSearchFlex">
+              <div className="authorFlex">
+                <h3 >Written By&nbsp;&nbsp;&nbsp;</h3>
+                <div className="authorFlex">{book.authors.map(author => {
+                  return (
+                    <div key={author}>
+                      <div><h3>&bull;{author}&nbsp;&nbsp;&nbsp;</h3></div>
+                    </div>
+                  )
+                })}
+                </div>
               </div>
               <div>{book.description}</div>
-              <img src={book.image} />
-              <div>{book.price}</div>
             </div>
-          )
-        })}
+            <div id="divide">
+              <hr></hr>
+            </div>
+          </div>
+        )
+      })}
 
-      </section>
-    </>
+    </section>
+
   )
 }
 
