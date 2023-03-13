0e376070044e42cd5f1caa3d8b57c33327b067d9
Kristen Schaefer <kristencschaefer@gmail.com>
2023-03-13 14:25:39 -0500

    added more data rendering

--- client/src/pages/BookPage.js
+++ client/src/pages/BookPage.js
@@ -12,6 +12,7 @@ const BookPage = () => {
   const fetchBook = async (bookID) => {
     const response = await axios.get(`https://www.googleapis.com/books/v1/volumes?q=isbn:${bookID}`);
     const data = response.data;
+    console.log(data)
     setBooks(data.items);
   }
 
@@ -23,10 +24,25 @@ const BookPage = () => {
     <>
       <div>
       {books.map((book) => (
-          <div key={book.id} style={{ display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center", padding: "10px", width: "150px" }}>
-            <p style={{ textAlign: "center", margin: 0 }}>{book.volumeInfo.title}</p>
-            <p style={{ textAlign: "center", margin: 0 }}>{book.volumeInfo.authors}</p>
+        <div>
+          <div key={book.id} style={{ display: "flex", flexDirection: "row", alignItems: "center", padding: "10px", width: "90%" }}>
             <img src={book.volumeInfo.imageLinks.thumbnail} alt={book.volumeInfo.title} style={{ maxWidth: "150px" }} />
+            <div style={{ display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center", padding: "10px" }}>
+            <h1 style={{ textAlign: "center", margin: 0 }}>{book.volumeInfo.title}</h1>
+            <h2 style={{ textAlign: "center", margin: 0 }}>{book.volumeInfo.subtitle}</h2>
+            </div>
+            
+            
+            </div>
+            <p style={{ textAlign: "left", margin: 0 }}>{book.volumeInfo.authors}</p>
+            <p style={{ textAlign: "left", margin: 0 }}>{book.volumeInfo.description}</p>
+            <p style={{ textAlign: "left", margin: 0 }}>{book.volumeInfo.language}</p>
+            <p style={{ textAlign: "left", margin: 0 }}>{book.volumeInfo.maturityRating}</p>
+            <p style={{ textAlign: "left", margin: 0 }}>{book.volumeInfo.pageCount}</p>
+            <p style={{ textAlign: "left", margin: 0 }}>{book.volumeInfo.publishedDate}</p>
+            <p style={{ textAlign: "left", margin: 0 }}>{book.volumeInfo.publisher}</p>
+            <p style={{ textAlign: "left", margin: 0 }}>Categories: {book.volumeInfo.categories}</p>
+            <p style={{ textAlign: "left", margin: 0 }}>ISBN: {book.volumeInfo.industryIdentifiers[0].identifier}</p>
             </div>
       ))
       }
