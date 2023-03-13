ecab7806e0d941a2640e5f12bd30a7703896de80
tesseir <tesseir.edu@gmail.com>
2023-03-13 10:26:30 -0500

    added add to cart functionality

---
+++ client/src/pages/CheckoutPage.js
@@ -0,0 +1,29 @@
+import { useState } from "react"
+import cookie from "js-cookie"
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../styles/global.css'
+
+const Checkout = (props) => {
+
+  return (
+    <>
+      <h1>Checkout</h1>
+      <div class="container">
+        <div class="row">
+
+          <div class="col-9">
+
+          </div>
+
+          <div class="col-3">
+
+          </div>
+
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Checkout
