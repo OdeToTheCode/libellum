bb6b23e2882af391d5d596b27569d3ad7630552b
Milo C <75094742+whotf1@users.noreply.github.com>
2023-03-13 12:39:52 -0500

    added some cool stuff

--- server/models/shoppingcart.js
+++ server/models/shoppingcart.js
@@ -1,58 +1,62 @@
 const {Schema, model} = require ("mongoose");
 const User = require("./User")
+const Book = require("./Book");
 const shoppingCartSchema = new Schema(
   {
 
-    User: {
-    type: string,
-    id: User.id,
+    user_id: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
     },
-    items: {
-      type: Array, 
-      name: string,
-      default: true,
-    itemDetails: new Schema ( 
-      {
-        id:{
-          type: string,
-          default: true,
-          minLength: 1,
-        },
-        selfLink: {
-          type: string,
-          default: true,
-          minLength: 1,
-        },
-        authors: {
-          type: string,
-          default: true,
-          minLength: 1,
-        },
-        catagories: {
-          type: string,
-          default: true,
-          minLength: 1,
-        },
-        description: {
-          type: string,
-          default: true,
-          minLength: 1,
-        },
-        imageLinks: {
-          type: Array,
-          default: true,
-          minLength: 0,
-        }
-      }
-    )
-    }
+    books: [
+      Book.schema
+    ]
+    // items: {
+    //   type: Array, 
+    //   name: string,
+    //   default: true,
+    // itemDetails: new Schema ( 
+    //   {
+    //     id:{
+    //       type: string,
+    //       default: true,
+    //       minLength: 1,
+    //     },
+    //     selfLink: {
+    //       type: string,
+    //       default: true,
+    //       minLength: 1,
+    //     },
+    //     authors: {
+    //       type: string,
+    //       default: true,
+    //       minLength: 1,
+    //     },
+    //     catagories: {
+    //       type: string,
+    //       default: true,
+    //       minLength: 1,
+    //     },
+    //     description: {
+    //       type: string,
+    //       default: true,
+    //       minLength: 1,
+    //     },
+    //     imageLinks: {
+    //       type: Array,
+    //       default: true,
+    //       minLength: 0,
+    //     }
+    //   }
+    // )
+    // }
   }
 );
 
 shoppingCart
   .virtual("getShoppingCartLength")
   .get(function () {
-    return this.items.legnth;
+    return this.books.length;
   });
 
   const shoppingCart = model("shoppingCart", shoppingCartSchema);
