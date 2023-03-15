4e45329f87715b3d34dcf1bd796ed45130836a82
tesseir <tesseir.edu@gmail.com>
2023-03-14 20:53:32 -0500

    altered model and seed

--- client/src/pages/CartPage.js
+++ client/src/pages/CartPage.js
@@ -1,4 +1,4 @@
-import { useState, useEffect ,useContext } from "react"
+import { useState, useEffect, useContext } from "react"
 import cookie from "js-cookie"
 
 import axios from "axios"
@@ -7,53 +7,53 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import '../styles/global.css'
 
 const Cart = (props) => {
-   const [cart, setCart] = useState([]);
-//   useEffect(() => {
-//     const shoppingCart = JSON.parse(localStorage.getItem("cart"));
-//     setCart(shoppingCart);
-//   }, [cart, setCart]);
+  const [cart, setCart] = useState([]);
+  //   useEffect(() => {
+  //     const shoppingCart = JSON.parse(localStorage.getItem("cart"));
+  //     setCart(shoppingCart);
+  //   }, [cart, setCart]);
 
 
 
-const fetchCart = async () => {
-  const response = await axios.get("/api/cart/6410a488e92a1b2cecf54d3a");
- setCart(response.data.books);
-}
+  const fetchCart = async () => {
+    const response = await axios.get("/api/cart/641122fd28efa32510553639");
+    setCart(response.data.books);
+  }
 
-useEffect(() => {
-  fetchCart();
-},[setCart, cart]) 
+  useEffect(() => {
+    fetchCart();
+  }, [setCart, cart])
 
-const renderCart = () => {
-  return cart.map((item) => {
-    return (
-      <div>
-        <h1>{item.title}</h1>
-        <h2>{item.author}</h2>
-        <h3>${item.price}</h3>
-        <input type = "checkbox" name = "delete" value = "delete" />
-      </div>
-    )
-  })
-}
+  const renderCart = () => {
+    return cart.map((item) => {
+      return (
+        <>
+          <div class="container">
+            <div class="row">
+              <div class="col-9">
+                <img src={item.image} alt={item.title} />
+                <h1>{item.title}</h1>
+                <h2>{item.author}</h2>
+
+              </div>
+              <div class="col-3">
+
+                <h3>${item.price}</h3>
+                <input type="checkbox" name="delete" value="delete" />
+
+              </div>
+            </div>
+          </div>
+        </>
+      )
+    })
+  }
 
   return (
     <>
-      <h1>Cart</h1>
-      <div class="container">
-        <div class="row">
+      <h1 style={{"text-align": "center"}}>Your Cart</h1>
+      <div>
         {renderCart()}
-
-
-          <div class="col-9">
-
-          </div>
-
-          <div class="col-3">
-
-          </div>
-
-        </div>
       </div>
     </>
   )
